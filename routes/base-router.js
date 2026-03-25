ec357a41feeb03477ed2c6d9c62fa9a1c91fa314
Judi <92621215+jxndela@users.noreply.github.com>
2026-03-25 17:58:54 +0800

    Song to Database CRUD
    Implement Song Creation, edit and deletion. Currently using uploader name only for verification

--- routes/base-router.js
+++ routes/base-router.js
@@ -5,10 +5,16 @@ const Song = require("./../models/songs-model.js");
 // Import routers
 const playlistRouter = require("./playlist-router.js");
 const eventsRouter = require("./events-router.js");
+const songsRouter = require("./songs-router.js");
 
 // Branching Route
+router.get("/", (req, res) => {
+    res.render("base");
+});
+
 router.use("/playlist", playlistRouter);
 router.use("/events", eventsRouter);
+router.use("/songs", songsRouter);
 
 // TODO: Remove these route when insert song popup is implemented.
 router.get("/song/search/:songID", async (req, res) => {
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const path = require("path");
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 
+
 // Load Env variable
 dotenv.config({ path: './config.env' });
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 
 // Middleware
 server.use(express.static(path.join(__dirname, "public")));
@@ -27,39 +29,6 @@ async function connectDB() {
     process.exit(1);
   }
 };
-
-//Load Model
-const User = require("./models/users-model");
-const Song = require("./models/songs-model");
-const Playlist = require("./models/playlists-model");
-const Review = require("./models/playlists-model");
-const Events = require("./models/events-model")
-
-// Middleware
-server.use(express.static(path.join(__dirname, "public")));
-server.use(express.urlencoded({ extended: true }));
-server.set('view engine', 'ejs');
-server.set("views", path.join(__dirname, "views"));
-
-// Routes
-server.get("/", (req, res) => {
-  res.render("base");
-});
-
-
-//testing
-
-server.get("/add-song", async (req, res) => {
-  await Song.create({
-    title: "Blinding Lights",
-    artist: "The Weeknd",
-    album: "After Hours",
-    genre: "Pop",
-    duration: 200
-  });
-
-  res.send("Song added to database");
-});
 
 // Initialize Server
 function startServer() {
